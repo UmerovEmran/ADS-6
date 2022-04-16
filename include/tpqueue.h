46553e636e7e9192bf2c51cc0c46530f5a18d827
UmerovEmran <99113996+UmerovEmran@users.noreply.github.com>
2022-04-16 13:09:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,26 +5,26 @@
 template<typename T, int size>
 class TPQueue {
  private:
-   T arr[10];
-   int first;
-   int last;
+    T arr[10];
+    int first;
+    int last;
 
  public:
-   TPQueue() :first(0), last(0) { }
-   void push(T vivod) {
-     int lastpp = last++;
-     for (bool i = false; i != true; ) {
-       if ((--lastpp >= first) && (arr[lastpp % size].prior < vivod.prior)) {
-         arr[(lastpp + 1) % size] = arr[lastpp % size];
-       } else {
-           i = true;
-         }
-     }
-     arr[(lastpp + 1) % size] = vivod;
-   }
-   T pop() {
-     return arr[(first++) % size];
-   }
+    TPQueue() :first(0), last(0) { }
+    void push(T vivod) {
+      int lastpp = last++;
+      for (bool i = false; i != true; ) {
+        if ((--lastpp >= first) && (arr[lastpp % size].prior < vivod.prior)) {
+          arr[(lastpp + 1) % size] = arr[lastpp % size];
+        } else {
+            i = true;
+          }
+      }
+      arr[(lastpp + 1) % size] = vivod;
+    }
+    T pop() {
+      return arr[(first++) % size];
+    }
 };
 
 struct SYM {
